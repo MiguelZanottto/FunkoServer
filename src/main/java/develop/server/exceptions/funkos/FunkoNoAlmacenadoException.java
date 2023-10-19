951ee86fe38a0b431195d4826338589e50aa4c85
Miguel Zanotto <migzanotto18@hotmail.com>
2023-10-19 23:24:45 +0200

    excepciones funkos agregadas

---
+++ src/main/java/develop/server/exceptions/funkos/FunkoNoAlmacenadoException.java
@@ -0,0 +1,7 @@
+package develop.server.exceptions.funkos;
+
+public class FunkoNoAlmacenadoException extends FunkoException{
+    public FunkoNoAlmacenadoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/develop/server/exceptions/funkos/FunkoNoEncotradoException.java
@@ -0,0 +1,7 @@
+package develop.server.exceptions.funkos;
+
+public class FunkoNoEncotradoException extends FunkoException {
+    public FunkoNoEncotradoException(String message) {
+        super(message);
+    }
+}
